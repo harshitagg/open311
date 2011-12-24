d617b382f0141406877fdf813125c0461fee53fe
Sadique Ali <sadiqalikm@gmail.com>
2011-12-24 13:08:56 +0530

    Moved tests for discovery to its own file.

---
+++ tests/test_discovery.py
@@ -0,0 +1,28 @@
+import unittest
+
+import main
+
+class TestServiceDiscovery(unittest.TestCase):
+
+    def setUp(self):
+        main.app.config['TESTING'] = True
+        self.app = main.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_service_discovery_xml(self):
+        response = self.app.get('/discovery.xml')
+        self.assertEquals(200, response.status_code)
+
+    def test_service_discovery_xml_content_type(self):
+        response = self.app.get('/discovery.xml')
+        self.assertEquals("text/xml; charset=utf-8", response.headers["Content-Type"])
+
+    def test_service_discovery_json(self):
+        response = self.app.get('/discovery.json')
+        self.assertEquals(200, response.status_code)
+
+    def test_service_discovery_json_content_type(self):
+        response = self.app.get('/discovery.json')
+        self.assertEquals("application/json; charset=utf-8", response.headers["Content-Type"])
