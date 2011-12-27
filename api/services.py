613ea86194cfb14dfb8f812559a805663f66248c
Harshit <harshit@harshit-VirtualBox.(none)>
2011-12-27 17:03:27 +0530

    accessing the services

--- api/services.py
+++ api/services.py
@@ -1,4 +1,7 @@
 from utils import content_type_for
+from webapp.access_services import access_service
+from utils import dict_to_lxml
+from lxml import etree
 
 class ServiceList(object):
 
@@ -6,7 +9,12 @@ class ServiceList(object):
         self.format = format
 
     def get(self):
-        return ""
+	root = etree.Element('services')
+	access_service_obj = access_service()
+	service_list = access_service_obj.getServiceList()
+	for service in service_list:
+		root.append(dict_to_lxml('service',services))
+        return etree.tostring(root, encoding='utf-8',xml_declaration=True)
 
     def content_type(self):
         return content_type_for(self.format)
@@ -32,4 +40,3 @@ class MultipleServiceRequest(object):
 
     def get(self):
         pass
-
