239a9ed4cc554bb557d1db15d087c34848dff845
Harshit <harshit@harshit-VirtualBox.(none)>
2011-12-28 12:13:23 +0530

    added support for json format

--- api/services.py
+++ api/services.py
@@ -1,3 +1,4 @@
+import json
 from utils import content_type_for
 from webapp.access_services import access_service
 from utils import dict_to_lxml
@@ -6,18 +7,30 @@ from lxml import etree
 class ServiceList(object):
 
     def __init__(self, format):
-        self.format = format
+	    self.formatter = _xml_formatter if format == 'xml' else _json_formatter
+	    self.format = format
 
     def get(self):
+	return self.formatter()
+
+    def content_type(self):
+        return content_type_for(self.format)
+
+def _xml_formatter():
 	root = etree.Element('services')
 	access_service_obj = access_service()
 	service_list = access_service_obj.getServiceList()
 	for service in service_list:
-		root.append(dict_to_lxml('service',services))
-        return etree.tostring(root, encoding='utf-8',xml_declaration=True)
+		root.append(dict_to_lxml('service',service))
+	return etree.tostring(root, encoding='utf-8',xml_declaration=True)
 
-    def content_type(self):
-        return content_type_for(self.format)
+def _json_formatter():
+	content=[]
+	access_service_obj = access_service()
+	service_list = access_service_obj.getServiceList()
+	for service in service_list:
+		content.append(service)
+	return json.dumps(content)
 
 class ServiceDefinition(object):
 
