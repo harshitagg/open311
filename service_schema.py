3db17d174bb147480bd3a72bdc4b84170e1c964c
Diptanu Choudhury <admin.nitjece@gmail.com>
2012-01-24 21:57:57 +0530

    Fixed indentation of another file

--- service_schema.py
+++ service_schema.py
@@ -7,113 +7,113 @@ from sqlalchemy.orm import relationship, backref
 Base = declarative_base()
 
 class Service(Base):
-        __tablename__='service'
+    __tablename__ = 'service'
 
-        id = Column(Integer, primary_key = True)
-	code = Column(Integer, nullable = False, unique = True)
-        name = Column(String, nullable = False)
-        descn = Column(String, nullable = False)
-        serv_metadata = Column(Boolean, nullable = False)
-        serv_type = Column(String, nullable = False)
-        group = Column(String, nullable = False)
+    id = Column(Integer, primary_key=True)
+    code = Column(Integer, nullable=False, unique=True)
+    name = Column(String, nullable=False)
+    descn = Column(String, nullable=False)
+    serv_metadata = Column(Boolean, nullable=False)
+    serv_type = Column(String, nullable=False)
+    group = Column(String, nullable=False)
 
 
 class Keywords(Base):
-	__tablename__='keywords'
+    __tablename__ = 'keywords'
 
-        id = Column(Integer, primary_key = True)
-	service_code = Column(Integer, ForeignKey('service.code'), nullable = False)
-	keyword = Column(String,nullable=False)
+    id = Column(Integer, primary_key=True)
+    service_code = Column(Integer, ForeignKey('service.code'), nullable=False)
+    keyword = Column(String, nullable=False)
 
-	UniqueConstraint('service_code', 'keyword', name = 'checkkeywords')
+    UniqueConstraint('service_code', 'keyword', name='checkkeywords')
 
 
 class Values(Base):
-	__tablename__ = 'values'
+    __tablename__ = 'values'
 
-        id = Column(Integer, primary_key = True)
-	key = Column(Integer, nullable = False)
-	name = Column(String, nullable = False)
-	attribute_code = Column(String, ForeignKey('attribute.code'), nullable = False)
-	service_code = Column(Integer, ForeignKey('service.code'), nullable = False)
-	
-	UniqueConstraint('key', 'attribute_code', 'service_code', name = 'checkvalues')
+    id = Column(Integer, primary_key=True)
+    key = Column(Integer, nullable=False)
+    name = Column(String, nullable=False)
+    attribute_code = Column(String, ForeignKey('attribute.code'), nullable=False)
+    service_code = Column(Integer, ForeignKey('service.code'), nullable=False)
+
+    UniqueConstraint('key', 'attribute_code', 'service_code', name='checkvalues')
 
 
 class Attributes(Base):
-	__tablename__ = 'attribute'
+    __tablename__ = 'attribute'
 
-        id = Column(Integer, primary_key = True)
-	variable = Column(Boolean, nullable = False)
-	code = Column(String, nullable = False)
-	datatype = Column(String, nullable = False)
-	required = Column(Boolean, nullable = False)
-	datatype_description = Column(String)
-	order = Column(Integer, nullable = False)
-	description = Column(String, nullable = False)
-	service_code = Column(Integer, ForeignKey('service.code'), nullable = False)
-	
-	UniqueConstraint('code', 'service_code', name = 'checkattributes')
+    id = Column(Integer, primary_key=True)
+    variable = Column(Boolean, nullable=False)
+    code = Column(String, nullable=False)
+    datatype = Column(String, nullable=False)
+    required = Column(Boolean, nullable=False)
+    datatype_description = Column(String)
+    order = Column(Integer, nullable=False)
+    description = Column(String, nullable=False)
+    service_code = Column(Integer, ForeignKey('service.code'), nullable=False)
+
+    UniqueConstraint('code', 'service_code', name='checkattributes')
 
 
 class Requests(Base):
-	__tablename__ = 'requests'
-	
-	id = Column(Integer, primary_key = True)
-	lat = Column(Float)
-	long = Column(Float)
-	address_string = Column(String)
-	address_id = Column(Integer)
-	email = Column(String)
-	device_id = Column(String)
-	account_id = Column(String)
-	first_name = Column(String)
-	last_name = Column(String)
-	phone = Column(BigInteger)
-	media_url = Column(String)
-	status = Column(String,  CheckConstraint("status in('open','close')"), nullable = False)
-	status_notes = Column(Text)
-	service_code = Column(Integer, ForeignKey('service.code'))
-	description = Column(Text)
-	agency_responsible = Column(String)
-	service_notice = Column(Text)
-	requested_datetime = Column(DateTime)
-	updated_datetime = Column(DateTime)
-	expected_datetime = Column(DateTime)
-	zipcode = Column(BigInteger)
+    __tablename__ = 'requests'
+
+    id = Column(Integer, primary_key=True)
+    lat = Column(Float)
+    long = Column(Float)
+    address_string = Column(String)
+    address_id = Column(Integer)
+    email = Column(String)
+    device_id = Column(String)
+    account_id = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    phone = Column(BigInteger)
+    media_url = Column(String)
+    status = Column(String, CheckConstraint("status in('open','close')"), nullable=False)
+    status_notes = Column(Text)
+    service_code = Column(Integer, ForeignKey('service.code'))
+    description = Column(Text)
+    agency_responsible = Column(String)
+    service_notice = Column(Text)
+    requested_datetime = Column(DateTime)
+    updated_datetime = Column(DateTime)
+    expected_datetime = Column(DateTime)
+    zipcode = Column(BigInteger)
 
 
 class RequestsId(Base):
-	__tablename__ = 'requests_id'
-	
-	service_request_id = Column(Integer, primary_key = True)
-	requests_id = Column(Integer, ForeignKey('requests.id'), nullable = False)
+    __tablename__ = 'requests_id'
+
+    service_request_id = Column(Integer, primary_key=True)
+    requests_id = Column(Integer, ForeignKey('requests.id'), nullable=False)
 
 
 class RequestsToken(Base):
-	__tablename__ = 'requests_token'
+    __tablename__ = 'requests_token'
 
-	token = Column(Integer, primary_key = True)
-	requests_id = Column(Integer, ForeignKey('requests.id'), nullable = False)
+    token = Column(Integer, primary_key=True)
+    requests_id = Column(Integer, ForeignKey('requests.id'), nullable=False)
 
 
 class RequestAttribue(Base):
-	__tablename__ = 'request_attribute'
+    __tablename__ = 'request_attribute'
 
-	id = Column(Integer, primary_key = True)
-	requests_id = Column(Integer, ForeignKey('requests.id'), nullable = False)
-	attribute_code = Column(Integer, ForeignKey('attribute.code'), nullable = False)
-	value = Column(String, ForeignKey('values.key'), nullable = False)
+    id = Column(Integer, primary_key=True)
+    requests_id = Column(Integer, ForeignKey('requests.id'), nullable=False)
+    attribute_code = Column(Integer, ForeignKey('attribute.code'), nullable=False)
+    value = Column(String, ForeignKey('values.key'), nullable=False)
 
-	UniqueConstraint('requests_id', 'attribute_code', 'value', name = 'checkrequestattribute')
-	
+    UniqueConstraint('requests_id', 'attribute_code', 'value', name='checkrequestattribute')
+
 
 class DbBase():
-	def __init__(self, engine):
-		self.engine = engine
+    def __init__(self, engine):
+        self.engine = engine
 
-	def create(self):
-		Base.metadata.create_all(self.engine)
+    def create(self):
+        Base.metadata.create_all(self.engine)
 
-	def drop(self):
-		Base.metadata.drop_all(self.engine)
+    def drop(self):
+        Base.metadata.drop_all(self.engine)
