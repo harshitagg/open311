0339e321fbcce1262a2ca9e89c8dd3642af5ec40
Diptanu Choudhury <admin.nitjece@gmail.com>
2012-01-27 22:30:06 +0530

    Added the Mongo Schema

--- api/models.py
+++ api/models.py
@@ -1,6 +1,19 @@
-from mongoengine.document import Document
-from mongoengine.fields import StringField
+from mongoengine.document import Document, EmbeddedDocument
+from mongoengine.fields import StringField, ListField, EmbeddedDocumentField, BooleanField, DecimalField
+
+class ServiceAttribute(EmbeddedDocument):
+    code = StringField(required=True)
+    variable = BooleanField(default=False)
+    order = DecimalField(required=True)
+    description = StringField(required=True)
 
 class Service(Document):
     definition = StringField(required=True)
     code = StringField(required=True)
+    name = StringField(required=True)
+    metadata = BooleanField(default=False)
+    type = StringField(default="realtime")
+    keywords = ListField(StringField)
+    group = StringField(required=False)
+    attributes = ListField(EmbeddedDocumentField(ServiceAttribute))
+    
